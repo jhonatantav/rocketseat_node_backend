7863c42bce6e56c7f252b8aae37440a6ff9cc68b
maahsweetjrt <jhony.tav@gmail.com>
2024-05-28 00:14:47 -0300

    alterações no no server.ts tirando o env.port

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { env } from './env'
 app
   .listen({
     host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
-    port: env.PORT,
+    port: PORT,
   })
   .then(() => {
     console.log('Servidor aberto')
